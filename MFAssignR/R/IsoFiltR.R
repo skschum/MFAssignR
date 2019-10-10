4d8ac9ddfff6a9e0e208f140cb65c7c4618941ad
skschum <skschum@mtu.edu>
2019-10-10 18:20:41 -0400

    Add files via upload

--- MFAssignR/R/IsoFiltR.R
+++ MFAssignR/R/IsoFiltR.R
@@ -26,6 +26,21 @@
 #' @param peaks data frame:
 #' The input 2 column data frame containing abundance and peak mass
 #'
+#' @param SN numeric:
+#' Sets the noise cut for the data, peaks below this value will not be evaluated
+#'
+#' @param Diffrat numeric:
+#' A value multiplied by the minimum allowed 13C ratio to expand the allowed range
+#'
+#' @param Sulfrat numeric:
+#' Sets the maximum 34S ratio that is allowed for matching
+#'
+#' @param Sulferr numeric:
+#' Sets the maximum allowed error (ppm) for 34S mass matching, default is 5
+#'
+#' @param Carberr numeric:
+#' Sets the maximum allowed error (ppm) for 13C mass matching, default is 5
+#'
 #'
 #' @return list(Monolist, Isolist):
 #'   Monolist - monoistopic and non-matched masses,
@@ -35,9 +50,9 @@
 #' IsoFiltR(peaks=df)
 #' @export
 
-#peaks <- DataX[DataX$intensity > 593*6,]
+#peaks <- DataX[DataX$Intensity > 3*20,]
 
-IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30) {
+IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30, Sulferr = 5, Carberr = 5) {
 
   names(peaks)[1] <- "Exp_mass"
   names(peaks)[2] <- "Abundance"
@@ -45,7 +60,7 @@ IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30) {
   Data1<- Data1[order(Data1$Exp_mass),]
 
   Sect <- ceiling(nrow(Data1))/10
-  Over <- round(Sect) * 0.05
+  Over <- round(Sect) * 0.15
 
   Data0 <- Data1[1:(Sect + Over),]
   Data2 <- Data1[Sect:(2*Sect+Over),]
@@ -61,81 +76,7 @@ IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30) {
 
   #Data frame set up
   End <- Data1
-  ###############
-  Sulflist <- list(Data0, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9, Data10)
 
-  IsoOutS_final <- data.frame(Exp_mass = -42, Abundance = -42)
-  MonoOutS_final <- data.frame(Exp_mass = -42, Abundance = -42)
-#i <- 2
-  for(i in 1:10){
-    I <- i
-    Data <- Sulflist[[I]]
-    Dataend <- Data
-    mass <- c(unlist(Data[c(1)]))
-    Data <- expand.grid(x = mass, y = mass)
-    names(Data)[1] <- "Exp_mass"
-    names(Data)[2] <- "Exp_mass1"
-
-    Data$mdiff <- Data$Exp_mass - Data$Exp_mass1
-    Data <- Data[Data$mdiff < 0,]
-    Data <- Data[Data$mdiff > -2 & Data$mdiff < -1.990,]
-    Data$S34_err <- abs(((Data$Exp_mass + 1.995797)-Data$Exp_mass1)/Data$Exp_mass1 * 10^6)
-    Data <- Data[Data$S34_err <= 5,]
-    Data <- Data[c(1:3)]
-
-    Data$KM <- Data$Exp_mass * (2 / 1.995797)
-    Data$KMD <- round((round(Data$Exp_mass) - Data$KM), 3)
-    Data$KMr <- Data$Exp_mass * ((round((14.01565/12))/((14.01565/12))))
-    Data$KMDr <- round((round(Data$KMr)-Data$KMr),3)
-
-    Data$KM1 <- Data$Exp_mass1 * (2 / 1.995797)
-    Data$KMD1 <- round((round(Data$Exp_mass1) - Data$KM1),3)
-    Data$KMr1 <- Data$Exp_mass1 * ((round((14.01565/12))/((14.01565/12))))
-    Data$KMDr1 <- round((round(Data$KMr1)-Data$KMr1),3)
-
-    Data$KMDrdiff <- Data$KMDr - Data$KMDr1
-    Data$KMDdiff <- Data$KMD - Data$KMD1
-
-    Pairs <- Data[abs(Data$KMDdiff) < 0.00249 & ((Data$KMDrdiff < -0.29051 & Data$KMDrdiff > -0.29349) |
-                                                       (Data$KMDrdiff < 0.70949 & Data$KMDrdiff > 0.7075)),]
-    ##############################################
-    MonoS <- Pairs[c(1)]
-    IsoS <- Pairs[c(2)]
-    names(IsoS)[1] <- "Exp_mass"
-
-    MonoS <- merge(MonoS, End, by.x = "Exp_mass", by.y = "Exp_mass")
-    #check <- unique(MonoS)
-    IsoS <- merge(IsoS, End, by.x = "Exp_mass", by.y = "Exp_mass")
-    #check2 <- unique(IsoS)
-    names(IsoS)[1] <- "Iso_mass"
-    names(IsoS)[2] <- "Iso_Abund"
-
-    ##Final Abundance Check
-    ##Be sure to check this as it is a risky point
-    Abund <- cbind(MonoS, IsoS)
-    Abunddummy <- data.frame(Exp_mass = -42, Abundance = -1, Iso_mass = -42, Iso_Abund = -1)
-    Abund <- rbind(Abund, Abunddummy)
-    Abund$ratio <- Abund$Iso_Abund / Abund$Abundance * 100
-
-    Abund <- Abund[Abund$ratio <= Sulfrat,]
-    Abund <- unique(Abund)
-
-    MonooutS <- Abund[c(1,2)]
-    IsooutS <- Abund[c(3,4)]
-    names(IsooutS)[1] <- "Exp_mass"
-    names(IsooutS)[2] <- "Abundance"
-
-    IsoOutS_final <- rbind(IsoOutS_final, IsooutS)
-    MonoOutS_final <- rbind(MonoOutS_final, MonooutS)
-
-  }
-
-  ##Test <- MonoOutS_final %>% mutate(Dups = duplicated(Exp_mass))
- # Test <- Test[Test$Dups == TRUE,]
-
-  IsoOutS_final <- unique(IsoOutS_final)
-  MonoOutS_final <- unique(MonoOutS_final)
-  #########################
   #Carbon Isotoping
 
   Carblist <- list(Data0, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9, Data10)
@@ -155,10 +96,12 @@ IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30) {
   Data <- expand.grid(x = mass, y = mass)
   names(Data)[1] <- "Exp_mass"
   names(Data)[2] <- "Exp_mass1"
+  #Data <- data.frame(Exp_mass = 384.09413, Exp_mass1 = 385.09672)
+
   Data$mdiff <- Data$Exp_mass - Data$Exp_mass1
   Data <- Data[Data$mdiff < -1.0015 & Data$mdiff > -1.005, ]
   Data$C13_err <- abs(((Data$Exp_mass + 1.0033548380)-Data$Exp_mass1)/Data$Exp_mass1 * 10^6)
-  Data <- Data[Data$C13_err <= 5,]
+  Data <- Data[Data$C13_err <= Carberr,]
   Data <- Data[c(1:3)]
 
   #Variable calculation
@@ -177,7 +120,8 @@ IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30) {
   Data$mdiff <- Data$Exp_mass - Data$Exp_mass1
 
   #The numbers chosen for filtering are based on the results of positive mode ESI for BB burning aerosol
-  Pairs <- Data[abs(Data$KMDdiff) <= 0.00149 & ((Data$KMDrdiff < -0.494501&Data$KMDrdiff > -0.4975) |
+  #Before change on 6/25/19, the second KMDrdiff was -0.4975
+  Pairs <- Data[abs(Data$KMDdiff) <= 0.00149 & ((Data$KMDrdiff < -0.494501&Data$KMDrdiff > -0.498001) |
                                                       (Data$KMDrdiff < 0.5045 & Data$KMDrdiff > 0.501501)),]
 
   Monopair <- Pairs[c(1,2)]
@@ -230,63 +174,65 @@ IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30) {
   Align1 <- merge(Align1, IsoAbund2, by.x = "Iso_mass2", by.y = "Iso_mass2", all= T)
   Align1 <- Align1[!is.na(Align1$Mono_mass),]
 
+  Align1 <- Align1[Align1$Iso_Abund1 < Align1$Mono_Abund,]
   #Diffrat <- 0
   #Calculating Abundance Ratios for QA purposes
   #Ratios estimated with Sisweb using multiples of C
 
-  Align1$Abund <- Align1$Iso_Abund1 / Align1$Mono_Abund
-  Align100 <- Align1[Align1$Abund < 0.0973 &  Align1$Mono_mass <=100,]
-  Align200 <- Align1[Align1$Abund < 0.1839 & Align1$Abund > Diffrat * 0.0433 & Align1$Mono_mass <=200&
-                       Align1$Mono_mass > 100,]
-  Align300 <- Align1[Align1$Abund < 0.2704 & Align1$Abund > Diffrat * 0.0757 & Align1$Mono_mass <=300&
-                       Align1$Mono_mass > 200,]
-  Align400 <- Align1[Align1$Abund < 0.3677 & Align1$Abund > Diffrat * 0.1082 & Align1$Mono_mass <=400&
-                       Align1$Mono_mass > 300,]
-  Align500 <- Align1[Align1$Abund < 0.4543 & Align1$Abund > Diffrat * 0.1406 & Align1$Mono_mass <=500&
-                       Align1$Mono_mass > 400,]
-  Align600 <- Align1[Align1$Abund < 0.5408 & Align1$Abund > Diffrat * 0.1731 & Align1$Mono_mass <=600&
-                       Align1$Mono_mass > 500,]
-  Align700 <- Align1[Align1$Abund < 0.6381 & Align1$Abund > Diffrat * 0.2055 & Align1$Mono_mass <=700&
-                       Align1$Mono_mass > 600,]
-  Align800 <- Align1[Align1$Abund < 0.7247 & Align1$Abund > Diffrat * 0.2379 & Align1$Mono_mass <=800&
-                       Align1$Mono_mass > 700,]
-  Align900 <- Align1[Align1$Abund < 0.8112 & Align1$Abund > Diffrat * 0.2704 & Align1$Mono_mass <=900&
-                       Align1$Mono_mass > 800,]
-  Align1000 <- Align1[Align1$Abund < 0.9085 & Align1$Abund > Diffrat * 0.3167 & Align1$Mono_mass <=1000&
-                        Align1$Mono_mass > 900,]
-
-  Align1 <- rbind(Align100, Align200, Align300, Align400, Align500, Align600, Align700,
-                  Align800, Align900, Align1000)
+  # Align1$Abund <- Align1$Iso_Abund1 / Align1$Mono_Abund
+  # Align100 <- Align1[Align1$Abund < 0.0973 &  Align1$Mono_mass <=100,]
+  # Align200 <- Align1[Align1$Abund < 0.1839 & Align1$Abund > Diffrat * 0.0433 & Align1$Mono_mass <=200&
+  #                      Align1$Mono_mass > 100,]
+  # Align300 <- Align1[Align1$Abund < 0.2704 & Align1$Abund > Diffrat * 0.0757 & Align1$Mono_mass <=300&
+  #                      Align1$Mono_mass > 200,]
+  # Align400 <- Align1[Align1$Abund < 0.3677 & Align1$Abund > Diffrat * 0.1082 & Align1$Mono_mass <=400&
+  #                      Align1$Mono_mass > 300,]
+  # Align500 <- Align1[Align1$Abund < 0.4543 & Align1$Abund > Diffrat * 0.1406 & Align1$Mono_mass <=500&
+  #                      Align1$Mono_mass > 400,]
+  # Align600 <- Align1[Align1$Abund < 0.5408 & Align1$Abund > Diffrat * 0.1731 & Align1$Mono_mass <=600&
+  #                      Align1$Mono_mass > 500,]
+  # Align700 <- Align1[Align1$Abund < 0.6381 & Align1$Abund > Diffrat * 0.2055 & Align1$Mono_mass <=700&
+  #                      Align1$Mono_mass > 600,]
+  # Align800 <- Align1[Align1$Abund < 0.7247 & Align1$Abund > Diffrat * 0.2379 & Align1$Mono_mass <=800&
+  #                      Align1$Mono_mass > 700,]
+  # Align900 <- Align1[Align1$Abund < 0.8112 & Align1$Abund > Diffrat * 0.2704 & Align1$Mono_mass <=900&
+  #                      Align1$Mono_mass > 800,]
+  # Align1000 <- Align1[Align1$Abund < 0.9085 & Align1$Abund > Diffrat * 0.3167 & Align1$Mono_mass <=1000&
+  #                       Align1$Mono_mass > 900,]
+  #
+  # Align1 <- rbind(Align100, Align200, Align300, Align400, Align500, Align600, Align700,
+  #                 Align800, Align900, Align1000)
 
   #Now to address the abundances of 13C_2 isotopes
   #Ratios estimated with Sisweb using multiples of CH4O
 
   IsoPair <- Align1[is.na(Align1$Iso_mass2),]
   IsoTri <- Align1[!is.na(Align1$Iso_mass2),]
-  IsoTri$Abund <- IsoTri$Iso_Abund2 / IsoTri$Mono_Abund
-
-  IsoTri100 <- IsoTri[IsoTri$Abund < 0.0042 &  IsoTri$Mono_mass <=100,]
-  IsoTri200 <- IsoTri[IsoTri$Abund < 0.0159 & IsoTri$Abund > Diffrat * 0.00007 & IsoTri$Mono_mass <=200&
-                        IsoTri$Mono_mass > 100,]
-  IsoTri300 <- IsoTri[IsoTri$Abund < 0.0351 & IsoTri$Abund > Diffrat * 0.0025 & IsoTri$Mono_mass <=300&
-                        IsoTri$Mono_mass > 200,]
-  IsoTri400 <- IsoTri[IsoTri$Abund < 0.0656 & IsoTri$Abund > Diffrat * 0.0053 & IsoTri$Mono_mass <=400&
-                        IsoTri$Mono_mass > 300,]
-  IsoTri500 <- IsoTri[IsoTri$Abund < 0.1007 & IsoTri$Abund > Diffrat * 0.0091 & IsoTri$Mono_mass <=500&
-                        IsoTri$Mono_mass > 400,]
-  IsoTri600 <- IsoTri[IsoTri$Abund < 0.1433 & IsoTri$Abund > Diffrat * 0.014 & IsoTri$Mono_mass <=600&
-                        IsoTri$Mono_mass > 500,]
-  IsoTri700 <- IsoTri[IsoTri$Abund < 0.2002 & IsoTri$Abund > Diffrat * 0.02 & IsoTri$Mono_mass <=700&
-                        IsoTri$Mono_mass > 600,]
-  IsoTri800 <- IsoTri[IsoTri$Abund < 0.2586 & IsoTri$Abund > Diffrat * 0.027 & IsoTri$Mono_mass <=800&
-                        IsoTri$Mono_mass > 700,]
-  IsoTri900 <- IsoTri[IsoTri$Abund < 0.3246 & IsoTri$Abund > Diffrat * 0.0351 & IsoTri$Mono_mass <=900&
-                        IsoTri$Mono_mass > 800,]
-  IsoTri1000 <- IsoTri[IsoTri$Abund < 0.4078 & IsoTri$Abund > Diffrat * 0.0475 & IsoTri$Mono_mass <=1000&
-                         IsoTri$Mono_mass > 900,]
-
-  IsoTri <- rbind(IsoTri100, IsoTri200, IsoTri300, IsoTri400, IsoTri500, IsoTri600, IsoTri700,
-                  IsoTri800, IsoTri900, IsoTri1000)
+  IsoTri <- IsoTri[IsoTri$Iso_Abund2 < IsoTri$Mono_Abund,]
+  # IsoTri$Abund <- IsoTri$Iso_Abund2 / IsoTri$Mono_Abund
+  #
+  # IsoTri100 <- IsoTri[IsoTri$Abund < 0.0042 &  IsoTri$Mono_mass <=100,]
+  # IsoTri200 <- IsoTri[IsoTri$Abund < 0.0159 & IsoTri$Abund > Diffrat * 0.00007 & IsoTri$Mono_mass <=200&
+  #                       IsoTri$Mono_mass > 100,]
+  # IsoTri300 <- IsoTri[IsoTri$Abund < 0.0351 & IsoTri$Abund > Diffrat * 0.0025 & IsoTri$Mono_mass <=300&
+  #                       IsoTri$Mono_mass > 200,]
+  # IsoTri400 <- IsoTri[IsoTri$Abund < 0.0656 & IsoTri$Abund > Diffrat * 0.0053 & IsoTri$Mono_mass <=400&
+  #                       IsoTri$Mono_mass > 300,]
+  # IsoTri500 <- IsoTri[IsoTri$Abund < 0.1007 & IsoTri$Abund > Diffrat * 0.0091 & IsoTri$Mono_mass <=500&
+  #                       IsoTri$Mono_mass > 400,]
+  # IsoTri600 <- IsoTri[IsoTri$Abund < 0.1433 & IsoTri$Abund > Diffrat * 0.014 & IsoTri$Mono_mass <=600&
+  #                       IsoTri$Mono_mass > 500,]
+  # IsoTri700 <- IsoTri[IsoTri$Abund < 0.2002 & IsoTri$Abund > Diffrat * 0.02 & IsoTri$Mono_mass <=700&
+  #                       IsoTri$Mono_mass > 600,]
+  # IsoTri800 <- IsoTri[IsoTri$Abund < 0.2586 & IsoTri$Abund > Diffrat * 0.027 & IsoTri$Mono_mass <=800&
+  #                       IsoTri$Mono_mass > 700,]
+  # IsoTri900 <- IsoTri[IsoTri$Abund < 0.3246 & IsoTri$Abund > Diffrat * 0.0351 & IsoTri$Mono_mass <=900&
+  #                       IsoTri$Mono_mass > 800,]
+  # IsoTri1000 <- IsoTri[IsoTri$Abund < 0.4078 & IsoTri$Abund > Diffrat * 0.0475 & IsoTri$Mono_mass <=1000&
+  #                        IsoTri$Mono_mass > 900,]
+  #
+  # IsoTri <- rbind(IsoTri100, IsoTri200, IsoTri300, IsoTri400, IsoTri500, IsoTri600, IsoTri700,
+  #                 IsoTri800, IsoTri900, IsoTri1000)
 
   FinalAlign <- rbind(IsoPair, IsoTri)
   #Now need to use the masses to determine which ones have not been accounted for.
@@ -306,7 +252,85 @@ IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30) {
   MonoOutC_final <- rbind(MonoOutC_final, MonoC)
 
 
-}
+  }
+
+  #######Sulfur Section
+  ###############
+  Sulflist <- list(Data0, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9, Data10)
+  #Sulflist <- list(Data1)
+  IsoOutS_final <- data.frame(Exp_mass = -42, Abundance = -42)
+  MonoOutS_final <- data.frame(Exp_mass = -42, Abundance = -42)
+  #i <- 1
+  for(i in 1:10){
+    I <- i
+    Data <- Sulflist[[I]]
+    Dataend <- Data
+    mass <- c(unlist(Data[c(1)]))
+    Data <- expand.grid(x = mass, y = mass)
+    names(Data)[1] <- "Exp_mass"
+    names(Data)[2] <- "Exp_mass1"
+
+    Data$mdiff <- Data$Exp_mass - Data$Exp_mass1
+    Data <- Data[Data$mdiff < 0,]
+    Data <- Data[Data$mdiff > -2 & Data$mdiff < -1.990,]
+    Data$S34_err <- abs(((Data$Exp_mass + 1.995797)-Data$Exp_mass1)/Data$Exp_mass1 * 10^6)
+    Data <- Data[Data$S34_err <= Sulferr,]
+    Data <- Data[c(1:3)]
+
+    Data$KM <- Data$Exp_mass * (2 / 1.995797)
+    Data$KMD <- round((round(Data$Exp_mass) - Data$KM), 3)
+    Data$KMr <- Data$Exp_mass * ((round((14.01565/12))/((14.01565/12))))
+    Data$KMDr <- round((round(Data$KMr)-Data$KMr),3)
+
+    Data$KM1 <- Data$Exp_mass1 * (2 / 1.995797)
+    Data$KMD1 <- round((round(Data$Exp_mass1) - Data$KM1),3)
+    Data$KMr1 <- Data$Exp_mass1 * ((round((14.01565/12))/((14.01565/12))))
+    Data$KMDr1 <- round((round(Data$KMr1)-Data$KMr1),3)
+
+    Data$KMDrdiff <- Data$KMDr - Data$KMDr1
+    Data$KMDdiff <- Data$KMD - Data$KMD1
+
+    Pairs <- Data[abs(Data$KMDdiff) < 0.00249 & ((Data$KMDrdiff < -0.29051 & Data$KMDrdiff > -0.29349) |
+                                                   (Data$KMDrdiff < 0.70949 & Data$KMDrdiff > 0.7075)),]
+    ##############################################
+    MonoS <- Pairs[c(1)]
+    IsoS <- Pairs[c(2)]
+    names(IsoS)[1] <- "Exp_mass"
+
+    MonoS <- merge(MonoS, End, by.x = "Exp_mass", by.y = "Exp_mass")
+    #check <- unique(MonoS)
+    IsoS <- merge(IsoS, End, by.x = "Exp_mass", by.y = "Exp_mass")
+    #check2 <- unique(IsoS)
+    names(IsoS)[1] <- "Iso_mass"
+    names(IsoS)[2] <- "Iso_Abund"
+
+    ##Final Abundance Check
+    ##Be sure to check this as it is a risky point
+    Abund <- cbind(MonoS, IsoS)
+    Abunddummy <- data.frame(Exp_mass = -42, Abundance = -1, Iso_mass = -42, Iso_Abund = -1)
+    Abund <- rbind(Abund, Abunddummy)
+    Abund$ratio <- Abund$Iso_Abund / Abund$Abundance * 100
+
+    Abund <- Abund[Abund$ratio <= Sulfrat,]
+    Abund <- unique(Abund)
+
+    MonooutS <- Abund[c(1,2)]
+    IsooutS <- Abund[c(3,4)]
+    names(IsooutS)[1] <- "Exp_mass"
+    names(IsooutS)[2] <- "Abundance"
+
+    IsoOutS_final <- rbind(IsoOutS_final, IsooutS)
+    MonoOutS_final <- rbind(MonoOutS_final, MonooutS)
+
+  }
+
+  ##Test <- MonoOutS_final %>% mutate(Dups = duplicated(Exp_mass))
+  # Test <- Test[Test$Dups == TRUE,]
+
+  IsoOutS_final <- unique(IsoOutS_final)
+  MonoOutS_final <- unique(MonoOutS_final)
+  ######End of Sulfur Section
+  #########################
 
   ##############################################
   IsoOutC1_final <- unique(IsoOutC1_final)
@@ -319,6 +343,37 @@ IsoFiltR <- function(peaks, SN = 0, Diffrat = 0.1, Sulfrat = 30) {
   MonoOutS_final$Tag <- "S"
   IsoOutS_final$Tag <- "S34"
 
+  ##Sulfur Check 6/24/19
+  #Filtering the 34S masses that do not have a corresponding 13C
+  Sulfs <- merge(MonoOutC_final, MonoOutS_final, by.x = c("Exp_mass", "Abundance"),
+                    by.y = c("Exp_mass", "Abundance"), all = TRUE)
+  Sulfgood <- Sulfs[!is.na(Sulfs$Tag.x) & !is.na(Sulfs$Tag.y),]
+  Sulfbad <- Sulfs[is.na(Sulfs$Tag.x) & !is.na(Sulfs$Tag.y),]
+  ###Comparison of "bad" 34S to C13
+  Carbs1 <- merge(IsoOutC1_final, Sulfbad, by.x = c("Exp_mass", "Abundance"),
+                 by.y = c("Exp_mass", "Abundance"), all = TRUE)
+  IsoOutC1_final <- Carbs1[(!is.na(Carbs1$Tag) & is.na(Carbs1$Tag.y)) |
+                             (!is.na(Carbs1$Tag) & !is.na(Carbs1$Tag.y)),]
+  IsoOutC1_final <- IsoOutC1_final[c(1:3)]
+  Sulfbad <- Carbs1[(is.na(Carbs1$Tag) & !is.na(Carbs1$Tag.y)),]
+  ###Comparison of "bad"34S to 2C13
+  Sulfbad <- Sulfbad[-3]
+  Carbs2 <- merge(IsoOutC2_final, Sulfbad, by.x = c("Exp_mass", "Abundance"),
+                  by.y = c("Exp_mass", "Abundance"), all = TRUE)
+  IsoOutC2_final <- Carbs2[(!is.na(Carbs2$Tag) & is.na(Carbs2$Tag.y)) |
+                             (!is.na(Carbs2$Tag) & !is.na(Carbs2$Tag.y)),]
+  IsoOutC2_final <- IsoOutC2_final[c(1:3)]
+  Sulfbad <- Carbs2[(is.na(Carbs2$Tag) & !is.na(Carbs2$Tag.y)),]
+  ##Sulfbad are the possible monoisotopic sulfur that had a matching 34S, but no 13C, and
+  ##these monoisotopic masses did not match as a possible 13C for a different mass, so they
+  ##are staying as they are and are being added to the "Sulfgood" mass list.
+  Sulfbad <- Sulfbad[c(1,2,5)]
+  names(Sulfbad)[3] <- "Tag"
+
+  Sulfgood <- Sulfgood[c(1,2,4)]
+  names(Sulfgood)[3] <- "Tag"
+
+  MonoOutS_final <- rbind(Sulfgood, Sulfbad)
   ###########
   #Set up the isotopes
   Isotopes <- rbind(IsoOutC1_final, IsoOutC2_final, IsoOutS_final)
