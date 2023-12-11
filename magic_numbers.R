4041b7599934446dac5c1548eca8283640119051
KristinaGomoryova <423436@mail.muni.cz>
2023-12-11 17:05:25 +0100

    magic numbers explanation

---
+++ magic_numbers.R
@@ -0,0 +1,14 @@
+# Separation of isotope mass for the resolution enhanced KMD
+## for 34S = 12 (obtained by trial and error)
+## for 13C = 21 (obtained from Zhang et al (2018))
+
+## KMDr difference S = -0.291 (32S) or 0.709 (34S)
+## KMDr difference C = -0.496 (12C) or 0.503 (13C)
+## difference is calculated by subtracting the KMDr value for the suspected polyisotopic mass from the monoisotopic mass
+
+# allowance for the masurment error
+## for the 13C: -0.4975 < KMDrDiff < - 0.494501, and 0.501501 < KMDrDiff < 0.5045
+## for the 34S: -0.29349 < KMDrDiff < -0.29051, and 0.7075 < KMDrDiff < 0.70949 
+
+
+
