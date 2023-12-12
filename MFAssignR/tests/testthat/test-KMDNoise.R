5f8a7187f53098cd1cde1b4750f1f354cc1f34a1
hechth <helge.hecht@recetox.muni.cz>
2023-12-12 13:18:53 +0100

    renamed cols in test case

--- MFAssignR/tests/testthat/test-KMDNoise.R
+++ MFAssignR/tests/testthat/test-KMDNoise.R
@@ -1,5 +1,6 @@
 test_that("KMDNoise works", {
   load("../../data/Raw_Neg_ML.rda")
+  names(Raw_Neg_ML) <- c("mass", "intensity")
   actual <- KMDNoise(Raw_Neg_ML)
   expect_equal(actual$Noise, 346.0706, tolerance = 1e-4)
 })
