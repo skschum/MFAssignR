b4ce7b01f6570a3dd318059aada6c1c5844b4156
KristinaGomoryova <423436@mail.muni.cz>
2023-12-12 11:18:07 +0100

    magic numbers explanation

--- magic_numbers.R
+++ magic_numbers.R
@@ -6,7 +6,7 @@
 ## KMDr difference C = -0.496 (12C) or 0.503 (13C)
 ## difference is calculated by subtracting the KMDr value for the suspected polyisotopic mass from the monoisotopic mass
 
-# allowance for the masurment error
+# allowance for the measurment error
 ## for the 13C: -0.4975 < KMDrDiff < - 0.494501, and 0.501501 < KMDrDiff < 0.5045
 ## for the 34S: -0.29349 < KMDrDiff < -0.29051, and 0.7075 < KMDrDiff < 0.70949 
 
